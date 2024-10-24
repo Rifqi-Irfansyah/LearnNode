221c64558ffb310433d7072749b2a8336ef9b0fc
Rifqi Irfansyah <rifqiirfansyah518@gmail.com>
2024-10-24 12:22:40 +0700

    feat: add CRUD API

--- response.js
+++ response.js
@@ -1,16 +1,13 @@
 const response = (statusCode, data, message, res) =>{
-    res.status(statusCode).json({
-        payload: {
-            status_code : statusCode,
-            datas: data,
-            message: message
-        },
+    res.json( statusCode,[{
+        payload: data,
+        message: message,
         pagination:{
             prev: "",
             next: "",
             max: ""
         }
-    })
+    }])
 }
 
 module.exports = response
